07fb5e56ded3653262d370384c0bc091627a5763
Gromitmugs <jediacp2000@gmail.com>
2022-08-29 16:29:46 +0700

    more fixes

--- go.mod
+++ go.mod
@@ -24,6 +24,6 @@ require (
 	github.com/agnivade/levenshtein v1.1.1 // indirect
 	github.com/go-chi/chi/v5 v5.0.7
 	github.com/uptrace/bun v1.1.7
-	github.com/uptrace/bun/dialect/sqlitedialect v1.1.7
+	github.com/uptrace/bun/dialect/pgdialect v1.1.7
 	github.com/uptrace/bun/driver/pgdriver v1.1.7
 )
--- server.go
+++ server.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"database/sql"
 	"log"
 	"net/http"
@@ -9,10 +10,11 @@ import (
 	"github.com/99designs/gqlgen/graphql/handler"
 	"github.com/99designs/gqlgen/graphql/playground"
 	"github.com/Gromitmugs/pokedex-graphql-sqlite/graph"
+	dbmodel "github.com/Gromitmugs/pokedex-graphql-sqlite/graph/db_model"
 	"github.com/Gromitmugs/pokedex-graphql-sqlite/graph/generated"
 	"github.com/go-chi/chi/v5"
 	"github.com/uptrace/bun"
-	"github.com/uptrace/bun/dialect/sqlitedialect"
+	"github.com/uptrace/bun/dialect/pgdialect"
 	"github.com/uptrace/bun/driver/pgdriver"
 )
 
@@ -27,7 +29,12 @@ func main() {
 	dsn := "postgres://user:12345@" + serviceName + ":5432/?sslmode=disable"
 	sqldb := sql.OpenDB(pgdriver.NewConnector(pgdriver.WithDSN(dsn)))
 
-	db := bun.NewDB(sqldb, sqlitedialect.New())
+	db := bun.NewDB(sqldb, pgdialect.New())
+
+	_, err := db.NewCreateTable().Model((*dbmodel.Pokemon)(nil)).Exec(context.Background())
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	srv := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: &graph.Resolver{
 		DB: graph.Database{
